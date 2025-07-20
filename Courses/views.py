25d60b6dcd7a52acab0269b00a01a5ab1540e8e4
gonulpolat <gp29polat@gmail.com>
2025-07-20 17:47:59 +0300

    İlk Html etiketleri eklendi

--- Courses/views.py
+++ Courses/views.py
@@ -9,7 +9,16 @@ data = {
 }
 
 def lists(request):
-    return HttpResponse('Kurs Listesi')
+    category_list = data.keys()
+    list_items = ""
+
+    for category in category_list:
+        redirect_url = reverse('courses_by_category_name', args=[category])
+        list_items += f"<li><a href='{redirect_url}'>{category}</a></li>"
+
+    html = f"<h1>Kurs Listesi</h1><br><ul>{list_items}</ul>"
+
+    return HttpResponse(html)
 
 def details(request, course_name):
     return HttpResponse(f'{course_name.capitalize()} Detay Sayfası')
@@ -19,12 +28,12 @@ def get_courses_by_category_name(request, category_name):
         category_text = data[category_name]
         return HttpResponse(category_text)
     except:
-        return HttpResponseNotFound('Yanlış Kategori Seçimi')
+        return HttpResponseNotFound('<h1>Yanlış Kategori Seçimi</h1>')
 
 def get_courses_by_category_id(request, category_id):
     category_key_list = list(data.keys())
     if category_id > len(category_key_list):
-        return HttpResponseNotFound('Yanlış Kategori Seçimi')
+        return HttpResponseNotFound('<h1>Yanlış Kategori Seçimi</h1>')
     category_name = category_key_list[category_id - 1]
 
     redirect_url = reverse('courses_by_category_name', args=[category_name])
