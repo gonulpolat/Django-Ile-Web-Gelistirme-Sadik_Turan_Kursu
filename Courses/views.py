542087d1ee1b33aa31466d71a327b393881549a5
gonulpolat <gp29polat@gmail.com>
2025-07-20 17:03:58 +0300

    Redirect (Yönlendirme) İşlemi
    url'e gelen int değer ve kategori ismine göre yönlendirmeler yapıldı

--- Courses/views.py
+++ Courses/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect, render
 
-# Create your views here.
+data = {
+    'programlama': 'Programlama Kategorisine Ait Kurslar',
+    'web-gelistirme': 'Web Geliştirme Kategorisine Ait Kurslar',
+    'mobil-uygulama': 'Mobil Uygulama Kategorisine Ait Kurslar',
+}
 
 def lists(request):
     return HttpResponse('Kurs Listesi')
@@ -10,16 +14,17 @@ def details(request, course_name):
     return HttpResponse(f'{course_name.capitalize()} Detay Sayfası')
 
 def get_courses_by_category_name(request, category_name):
-    text = ""
-    if category_name == "programlama":
-        text = "Programlama Kategorisine Ait Kurslar"
-    elif category_name == "web-gelistirme":
-        text = "Web Geliştirme Kategorisine Ait Kurslar"
-    elif category_name == "mobil-uygulama":
-        text = "Mobil Uygulama Kategorisine Ait Kurslar"
-    else:
-        text = "Yanlış Kategori Seçimi"
-    return HttpResponse(text)
+    try: 
+        category_text = data[category_name]
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound('Yanlış Kategori Seçimi')
 
 def get_courses_by_category_id(request, category_id):
-    return HttpResponse(category_id)
+    # return HttpResponseRedirect('programlama')
+    # return redirect('/kurs/kategori/programlama')
+    category_key_list = list(data.keys())
+    if category_id > len(category_key_list):
+        return HttpResponseNotFound('Yanlış Kategori Seçimi')
+    redirect_text = category_key_list[category_id - 1]
+    return redirect(f'/kurs/kategori/{redirect_text}')
