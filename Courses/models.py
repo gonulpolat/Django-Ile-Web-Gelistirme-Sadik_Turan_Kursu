5edc66f5bd4fbb872ef02ab6f77ed7c83dd184bd
gonulpolat <gp29polat@gmail.com>
2025-07-22 17:01:02 +0300

    İlk model oluşturuldu
    Migrationlar uygulandı
    
    Database takip edilen dosyalardan çıkarıldı

--- Courses/models.py
+++ Courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    descriptipon = models.TextField(null=True)
+    imageUrl = models.CharField(max_length=50, blank=True)
+    date = models.DateField(null=True, blank=True)
+    isActive = models.BooleanField()
+    isUpdated = models.BooleanField()
