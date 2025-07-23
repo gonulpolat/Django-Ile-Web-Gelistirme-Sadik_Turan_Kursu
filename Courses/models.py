62edefe3fa0d66e0f5ee3ee9dc193c2e154358be
gonulpolat <gp29polat@gmail.com>
2025-07-23 16:17:01 +0300

    Yönetim paneli aktif edildi. Superuser oluşturuldu
    Modeller admin paneline eklendi

--- Courses/models.py
+++ Courses/models.py
@@ -10,15 +10,28 @@ class Course(models.Model):
     date = models.DateField(null=True, blank=True)
     isActive = models.BooleanField()
     isUpdated = models.BooleanField()
-    slug = models.SlugField(db_index=True, default='', null=False, unique=True)    # yani önceki verilere boş karakter gir
+    slug = models.SlugField(blank=True, db_index=True, default='', editable=False, null=False, unique=True)
+    """
+    blank    -> form ile ilgili
+                True: boş olabilir / False: boş olamaz
+    null     -> veri tabanı ile ilgili
+                True: boş olabilir / False: boş olamaz
+    default  -> veri tabanı ile ilgili
+    editable -> form ile ilgili
+                True: alan görünür ve değer girilebilir / False: form üzerinde görünmüyor
+    """ 
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(args, kwargs)
 
     def __str__(self):
+        # http://127.0.0.1:8000/admin/Courses/course/    => isimler bu fonksiyondan geliyor
         return f"{self.title}"
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"{self.name}"
