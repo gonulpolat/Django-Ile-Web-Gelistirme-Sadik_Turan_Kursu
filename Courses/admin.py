fef5ffdd3013ad9af21b7a91d9a070f78f5f9f81
gonulpolat <gp29polat@gmail.com>
2025-07-23 21:01:13 +0300

    1 to many ilişki admin panelinde uygulandı

--- Courses/admin.py
+++ Courses/admin.py
@@ -4,18 +4,17 @@ from .models import Category, Course
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'isActive', 'isUpdated')    # http://127.0.0.1:8000/admin/Courses/course/ sayfasında görünecek alanlar
-    list_display_links = ('title', 'slug')                       # başta sadece title linkti
-    readonly_fields = ('slug',)                                  # model içinde slug alanı için editable=False dediğin zaman slug alanı görünmüyordu, şimdi görünür ancak değiştirilemez
-    list_filter = ('isActive', 'isUpdated')
-    list_editable = ('isActive',)                                # kurs detayına gitmeden değitirmek için
+    list_display = ('title', 'slug', 'isActive', 'isUpdated')    
+    list_display_links = ('title', 'slug')                      
+    prepopulated_fields = {'slug': ('title',),}
+    list_filter = ('isActive', 'isUpdated', 'category')
+    list_editable = ('isActive',)                                
     search_fields = ('title', 'description')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',),}
 
 
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Course, CourseAdmin)
--- Courses/models.py
+++ Courses/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=60)
+    slug = models.SlugField(default='', null=False, unique=True, db_index=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -17,12 +17,8 @@ class Course(models.Model):
     date = models.DateField(auto_now=True, null=True, blank=True)
     isActive = models.BooleanField()
     isUpdated = models.BooleanField()
-    slug = models.SlugField(blank=True, db_index=True, default='', editable=False, null=False, unique=True)
-    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE, related_name='kurslar')        # Bir kategori silindiğinde onla ilişikili olan tüm Kurslar da silinir. default parametresi veri tabanında kayıt olduğu için kullanıldı
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(args, kwargs)
+    slug = models.SlugField(blank=True, db_index=True, default='', null=False, unique=True)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE, related_name='kurslar')
 
     def __str__(self):
         return f"{self.title}"
