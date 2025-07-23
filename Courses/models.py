8a91faca00c453a5cf1289defc1e954c06f48d2c
gonulpolat <gp29polat@gmail.com>
2025-07-23 17:19:06 +0300

    Terminalden 1 to many ilişkilerde kayıt ekleme ve kayıt güncelleme yapıldı

--- Courses/models.py
+++ Courses/models.py
@@ -14,7 +14,7 @@ class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True)
     imageUrl = models.CharField(max_length=50, blank=True)
-    date = models.DateField(null=True, blank=True)
+    date = models.DateField(auto_now=True, null=True, blank=True)
     isActive = models.BooleanField()
     isUpdated = models.BooleanField()
     slug = models.SlugField(blank=True, db_index=True, default='', editable=False, null=False, unique=True)
