699e511b6cdf865deb7c2a0e22b0b4052d75c8d0
gonulpolat <gp29polat@gmail.com>
2025-07-27 06:49:21 +0300

    Uygulama için url'ler eklendi
    Navbar'a Giriş/Kayıt/Çıkış linkleri eklendi.
    
    Navbar responsive halde getirildi.

--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
+def user_login(request):
+    return render(request, 'account/login.html')
+
+
+def user_register(request):
+    return render(request, 'account/register.html')
+
+
+def user_logout(request):
+    return redirect('index')
+
