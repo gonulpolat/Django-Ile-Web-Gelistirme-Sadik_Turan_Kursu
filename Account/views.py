0958f4f30910bbf42e65741062f1a62708ca2c70
gonulpolat <gp29polat@gmail.com>
2025-07-27 07:59:27 +0300

    User Register

--- Account/views.py
+++ Account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
 # Create your views here.
@@ -25,7 +26,42 @@ def user_login(request):
 
 
 def user_register(request):
-    return render(request, 'account/register.html')
+
+    if request.method == 'POST':
+        username = request.POST['username'].strip()
+        email = request.POST['email']
+        password = request.POST['password']
+        repassword = request.POST['repassword']
+
+        if password != repassword:
+            return render(request, 'account/register.html', {
+                'error': 'Parola eşleşmiyor.',
+                'username': username,
+                'email': email,
+            })
+        
+        if User.objects.filter(username = username).exists():
+            return render(request, 'account/register.html', {
+                'error': 'Username kullanılıyor.',
+                'email': email,
+                'password': password,
+                'repassword': repassword,
+            })
+        
+        if User.objects.filter(email = email).exists():
+            return render(request, 'account/register.html', {
+                'error': 'Email kullanılıyor.',
+                'username': username,
+                'password': password,
+                'repassword': repassword,
+            })
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return redirect('user_login')
+
+    else:
+        return render(request, 'account/register.html')
 
 
 def user_logout(request):
