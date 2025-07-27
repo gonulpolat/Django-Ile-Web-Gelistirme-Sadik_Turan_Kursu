0b4d5a1e4d6764caa0ca0b19a31d95b12bdcc3ce
gonulpolat <gp29polat@gmail.com>
2025-07-27 07:07:41 +0300

    User Login

--- Account/views.py
+++ Account/views.py
@@ -1,9 +1,23 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 
 # Create your views here.
 
 def user_login(request):
-    return render(request, 'account/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'account/login.html', {
+                'error': 'Username ya da parola yanlış!',
+            })
+    else:
+        return render(request, 'account/login.html')
 
 
 def user_register(request):
